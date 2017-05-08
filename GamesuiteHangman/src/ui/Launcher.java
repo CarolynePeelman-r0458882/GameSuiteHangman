081179dd22b8131c58ec3a48bcc6a6a7cf7867d0
thecoder <thecoder@iswleuven.be>
2017-05-08 15:29:08 +0200

    Test

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -2,15 +2,15 @@ package ui;
 
 import javax.swing.JOptionPane;
 
-import domain.Speler;
+//import domain.Speler;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-		Speler speler = new Speler(naam);
+		//Speler speler = new Speler(naam);
 
-		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		//JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 	}
 
 }
