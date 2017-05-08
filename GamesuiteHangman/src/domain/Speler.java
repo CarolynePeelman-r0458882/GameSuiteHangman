ff74078044d17505e275d39a18f3f578436afd9a
MAARTENLAPTOP\Maarten Van den hof <maartenvdh@live.be>
2017-05-08 15:50:21 +0200

    Speler

--- GamesuiteHangman/src/domain/Speler.java
+++ GamesuiteHangman/src/domain/Speler.java
@@ -1,5 +1,27 @@
 package domain;
 
 public class Speler {
-
+	private String naam;
+	private int score;
+	
+	
+	
+	public Speler(String naam) {
+		super();
+		this.naam = naam;
+	}
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
 }
