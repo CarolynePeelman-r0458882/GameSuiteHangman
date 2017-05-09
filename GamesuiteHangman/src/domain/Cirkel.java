b85d592517a9933e408f778c6098a703b1689b69
Carolyne Peelman <carolynepeelman@Carolynes-MacBook-Pro.local>
2017-05-09 13:16:11 +0200

    getOmhullende bij Cirkel

--- GamesuiteHangman/src/domain/Cirkel.java
+++ GamesuiteHangman/src/domain/Cirkel.java
@@ -3,40 +3,55 @@ package domain;
 public class Cirkel extends Vorm {
 	private int radius;
 	private Punt middelpunt;
-	
-	public Cirkel(Punt middelpunt, int radius){
+
+	public Cirkel(Punt middelpunt, int radius) {
 		setMiddelpunt(middelpunt);
 		setRadius(radius);
 	}
-	
-	public void setMiddelpunt(Punt middelpunt){
-		if(middelpunt == null) throw new DomainException("Middelpunt mag niet leeg zijn.");
+
+	public void setMiddelpunt(Punt middelpunt) {
+		if (middelpunt == null)
+			throw new DomainException("Middelpunt mag niet leeg zijn.");
 		this.middelpunt = middelpunt;
 	}
-	
-	private void setRadius(int radius){
-		if(radius <= 0) throw new DomainException("Radius moet groter dan 0 zijn.");
+
+	private void setRadius(int radius) {
+		if (radius <= 0)
+			throw new DomainException("Radius moet groter dan 0 zijn.");
 		this.radius = radius;
 	}
-	
-	public Punt getMiddelpunt(){
+
+	public Punt getMiddelpunt() {
 		return this.middelpunt;
 	}
-	
-	public int getRadius(){
+
+	public int getRadius() {
 		return this.radius;
 	}
-	
+
 	@Override
-	public boolean equals(Object object){
-		if(object == null) return false;
-		if(!(object instanceof Cirkel)) return false;
+	public boolean equals(Object object) {
+		if (object == null)
+			return false;
+		if (!(object instanceof Cirkel))
+			return false;
 		Cirkel cirkel = (Cirkel) object;
 		return this.radius == cirkel.getRadius() && this.getMiddelpunt() == cirkel.getMiddelpunt();
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		return "Cirkel[middelpunt: " + this.middelpunt + ", straal: " + Integer.toString(radius) + "]";
+	}
+
+	@Override
+	public String getOmhullende() {
+		int minX = middelpunt.getX() - radius;
+		int minY = middelpunt.getY() - radius;
+		int breedte = radius*2;
+		int hoogte = radius*2;
+		Omhullende omhullende = new Omhullende(new Punt(minX, minY), breedte, hoogte);
+		String cirkelString = "Cirkel: middelpunt " + middelpunt.toString() + " - straal: " + radius;
+		return cirkelString + omhullende.toString();
 	}
 }
