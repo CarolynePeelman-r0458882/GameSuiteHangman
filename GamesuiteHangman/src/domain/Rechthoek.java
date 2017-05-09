6949e6ac2a311e1556c27dfa18fa32aa633010d6
Pitlord <milly.boghe@gmail.com>
2017-05-09 15:49:34 +0200

    fix the fucking merge

--- GamesuiteHangman/src/domain/Rechthoek.java
+++ GamesuiteHangman/src/domain/Rechthoek.java
@@ -1,77 +1,62 @@
 package domain;
 
 public class Rechthoek extends Vorm {
-	private int breedte;
-	private int hoogte;
-	private Punt linkerBovenhoek;
-
-	public Rechthoek(Punt linkerbovenhoek, int breedte, int hoogte) {
-		setBreedte(breedte);
-		setHoogte(hoogte);
-		setLinkerBovenhoek(linkerbovenhoek);
-	}
-
-	public Punt getLinkerBovenhoek() {
-		return linkerBovenhoek;
-	}
-
-	private void setLinkerBovenhoek(Punt hoek) {
-		if (hoek == null) {
-			throw new DomainException("hoek mag niet null zijn");
+private int breedte;
+private int hoogte;
+private Punt linkerBovenhoek;
+public Rechthoek( Punt linkerbovenhoek, int breedte, int hoogte){
+	setBreedte(breedte);
+	setHoogte(hoogte);
+	setLinkerBovenhoek(linkerbovenhoek);
+}
+@Override
+public boolean equals(Object o){
+	boolean gelijk = false;
+	if(o instanceof Rechthoek){
+		Rechthoek rechthoek = (Rechthoek)o;
+		if(this.getBreedte()==rechthoek.getBreedte()&&this.getHoogte()==rechthoek.getHoogte()&&this.getLinkerBovenhoek()==rechthoek.getLinkerBovenhoek()){
+			gelijk = true;
 		}
-		this.linkerBovenhoek = hoek;
 	}
-
-	public int getBreedte() {
-		return breedte;
+	return gelijk;
+}
+@Override
+public String toString(){
+	return "breedte is gelijk aan " + this.getBreedte()+" en hoogte is gelijk aan "+ this.getHoogte();
+}
+public Punt getLinkerBovenhoek(){
+	return linkerBovenhoek;
+}
+public void setLinkerBovenhoek(Punt hoek){
+	if(hoek==null){
+		throw new DomainException("hoek mag niet null zijn");
 	}
-
-	private void setBreedte(int breedte) {
-		if (breedte <= 0) {
-			throw new DomainException("breedte mag niet kleiner dan 0 zijn");
-		}
-		this.breedte = breedte;
+	this.linkerBovenhoek=hoek;
+}
+public int getBreedte() {
+	return breedte;
+}
+public void setBreedte(int breedte) {
+	if(breedte<=0){
+		throw new DomainException("breedte mag niet kleiner dan 0 zijn");
 	}
+	this.breedte = breedte;
 
-	public int getHoogte() {
-		return hoogte;
+}
+public int getHoogte() {
+	return hoogte;
+}
+public void setHoogte(int hoogte) {
+	if(hoogte<=0){
+		throw new DomainException("hoogte mag niet kleiner dan 0 zijn");
 	}
+	this.hoogte = hoogte;
+}
+@Override
+public String getOmhullende() {
+	Omhullende omhullende = new Omhullende(getLinkerBovenhoek(), getBreedte(), getHoogte());
+	return omhullende.getOmhullende();
+}
 
-	private void setHoogte(int hoogte) {
-		if (hoogte <= 0) {
-			throw new DomainException("hoogte mag niet kleiner dan 0 zijn");
-		}
-		this.hoogte = hoogte;
-
-	}
-<<<<<<< HEAD
-=======
-
->>>>>>> 635b2888962609a4800e3d349425b40d44748fb6
-
-	@Override
-	public boolean equals(Object o) {
-		boolean gelijk = false;
-		if (o instanceof Rechthoek) {
-			Rechthoek rechthoek = (Rechthoek) o;
-			if (this.getBreedte() == rechthoek.getBreedte() && this.getHoogte() == rechthoek.getHoogte()
-					&& this.getLinkerBovenhoek() == rechthoek.getLinkerBovenhoek()) {
-				gelijk = true;
-			}
-		}
-		return gelijk;
-	}
-
-	@Override
-	public String toString() {
-		return "Rechthoek: positie: " + getLinkerBovenhoek().toString() + " - breedte: " + getBreedte() + " - hoogte: "
-				+ getHoogte();
-	}
-
-	@Override
-	public String getOmhullende() {
-		Omhullende omhullende = new Omhullende(getLinkerBovenhoek(), getBreedte(), getHoogte());
-		return omhullende.getOmhullende();
-	}
 
 }
