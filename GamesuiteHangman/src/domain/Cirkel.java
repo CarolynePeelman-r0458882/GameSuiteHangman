8bc4eb84375c06dfb7664e3f94715119171e2df2
thecoder <thecoder@iswleuven.be>
2017-05-09 10:37:45 +0200

    Cirkel.class (Doener: Tom, Denker: Brend)

--- GamesuiteHangman/src/domain/Cirkel.java
+++ GamesuiteHangman/src/domain/Cirkel.java
@@ -1,5 +1,36 @@
 package domain;
 
 public class Cirkel {
-
+	private int radius;
+	private Punt middelpunt;
+	
+	public Cirkel(Punt middelpunt, int radius){
+		setMiddelpunt(middelpunt);
+		setRadius(radius);
+	}
+	
+	public void setMiddelpunt(Punt middelpunt){
+		if(middelpunt == null) throw new DomainException("Middelpunt mag niet leeg zijn.");
+		this.middelpunt = middelpunt;
+	}
+	
+	private void setRadius(int radius){
+		if(radius <= 0) throw new IllegalArgumentException("Radius moet groter dan 0 zijn.");
+		this.radius = radius;
+	}
+	
+	public Punt getMiddelpunt(){
+		return this.middelpunt;
+	}
+	
+	public int getRadius(){
+		return this.radius;
+	}
+	
+	public boolean equals(Object object){
+		if(object == null) return false;
+		if(!(object instanceof Cirkel)) return false;
+		Cirkel cirkel = (Cirkel) object;
+		return this.radius == cirkel.getRadius() && this.getMiddelpunt() == cirkel.getMiddelpunt();
+	}
 }
