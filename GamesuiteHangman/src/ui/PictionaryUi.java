77ecbb292e48f83ef8670c86b5b27fd377ada13f
thecoder <thecoder@iswleuven.be>
2017-05-09 10:12:06 +0200

    Update PictionayUI.class

---
+++ GamesuiteHangman/src/ui/PictionaryUi.java
@@ -0,0 +1,14 @@
+package ui;
+import domain.*;
+
+public class PictionaryUi {
+	private Speler speler;
+	
+	public PictionaryUi(Speler speler){
+		this.speler = speler;
+	}
+	
+	public void showMenu(){
+		
+	}
+}
