8b37f03255607b979533e14582c48d46d0aa4876
Carolyne Peelman <carolynepeelman@Carolynes-MacBook-Pro.local>
2017-05-09 13:13:51 +0200

    getOmhullende bij Driehoek

--- GamesuiteHangman/src/domain/Driehoek.java
+++ GamesuiteHangman/src/domain/Driehoek.java
@@ -1,45 +1,86 @@
 package domain;
 
-public class Driehoek extends Vorm{
+public class Driehoek extends Vorm {
 	private Punt punt1;
 	private Punt punt2;
 	private Punt punt3;
-	
-	public Driehoek(Punt punt1, Punt punt2, Punt punt3){
+
+	public Driehoek(Punt punt1, Punt punt2, Punt punt3) {
 		setHoekpunten(punt1, punt2, punt3);
 	}
-	
-	public void setHoekpunten(Punt punt1, Punt punt2, Punt punt3){
-		if(punt1==null||punt2==null||punt3==null) throw new DomainException("De 3 punten mogen niet leeg zijn.");
+
+	public void setHoekpunten(Punt punt1, Punt punt2, Punt punt3) {
+		if (punt1 == null || punt2 == null || punt3 == null)
+			throw new DomainException("De 3 punten mogen niet leeg zijn.");
 		this.punt1 = punt1;
 		this.punt2 = punt2;
 		this.punt3 = punt3;
 	}
-	
-	public Punt getHoekPunt1(){
+
+	public Punt getHoekPunt1() {
 		return this.punt1;
 	}
-	
-	public Punt getHoekPunt2(){
+
+	public Punt getHoekPunt2() {
 		return this.punt2;
 	}
-	
-	public Punt getHoekPunt3(){
+
+	public Punt getHoekPunt3() {
 		return this.punt3;
 	}
 
 	@Override
-	public boolean Equals(Object object) {
-		if(object==null) return false;
-		if(!(object instanceof Driehoek)) return false;
+	public boolean equals(Object object) {
+		if (object == null)
+			return false;
+		if (!(object instanceof Driehoek))
+			return false;
 		Driehoek driehoek = (Driehoek) object;
-		return this.getHoekPunt1().equals(driehoek.getHoekPunt1()) && this.getHoekPunt2().equals(driehoek.getHoekPunt2()) && this.getHoekPunt3().equals(driehoek.getHoekPunt3());
+		return this.getHoekPunt1().equals(driehoek.getHoekPunt1())
+				&& this.getHoekPunt2().equals(driehoek.getHoekPunt2())
+				&& this.getHoekPunt3().equals(driehoek.getHoekPunt3());
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		return "Driehoek[punt1: " + punt1 + ", punt2: " + punt2 + ", punt3: " + punt3 + "]";
 	}
-	
-	
+
+	@Override
+	public String getOmhullende() {
+		int minY = minimum(punt1.getY(), punt2.getY(), punt3.getY());
+		int minX = minimum(punt1.getX(), punt2.getX(), punt3.getX());
+		int maxY = maximum(punt1.getY(), punt2.getY(), punt3.getY());
+		int maxX = maximum(punt1.getX(), punt2.getX(), punt3.getX());
+		int breedte = maxX - minX;
+		int hoogte = maxY - minY;
+		Omhullende omhullende = new Omhullende(new Punt(minX, minY), breedte, hoogte);
+		String driehoekString = "Driehoek: hoekpunt1: " + punt1.toString() + " - hoekpunt2: " + punt2.toString()
+				+ " - hoekpunt3: " + punt3.toString();
+		return driehoekString + omhullende.toString();
+
+	}
+
+	private int minimum(int waarde1, int waarde2, int waarde3) {
+		int min = waarde1;
+		if (waarde2 < min) {
+			min = waarde2;
+		}
+		if (waarde3 < min) {
+			min = waarde3;
+		}
+		return min;
+	}
+
+	private int maximum(int waarde1, int waarde2, int waarde3) {
+		int max = waarde1;
+		if (waarde2 > max) {
+			max = waarde2;
+		}
+		if (waarde3 > max) {
+			max = waarde3;
+		}
+		return max;
+	}
+
 }
