74cdd57626c13e3035d56e19f48714b0e50c4a55
Carolyne Peelman <carolynepeelman@Carolynes-MacBook-Pro.local>
2017-05-10 13:37:19 +0200

    no message

--- GamesuiteHangman/src/db/DbException.java
+++ GamesuiteHangman/src/db/DbException.java
@@ -1,6 +1,8 @@
 package db;
 
 public class DbException extends IllegalArgumentException{
+	private static final long serialVersionUID = -6796749518778847734L;
+
 	public DbException(){
 		super();
 	}
